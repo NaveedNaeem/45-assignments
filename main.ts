91e263646571298fded23cb961b5564e95f5e321
NaveedNaeem <naveednaeem1990@gmail.com>
2024-05-03 11:55:56 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,20 @@
+//Task 01.
+//Install Node.js, TypeScript and VS Code on your computer.
+
+//Node.js install on your computer.
+ let Node_js ="node -version 20.11";
+ console.log(Node_js);//print value
+
+//Typescript install on your computer.
+ let Type_script = "tsc -version 5.3.3";
+ console.log(Type_script);//print value
+
+//Vscode install on your computer.
+ let Vs_code= "Version 1.86..2";
+ console.log(Vs_code);//print value
+
+//Three software intall on your computer. 
+
+//Task 02
+let personName : string = "robert";
+console.log(`Hello ${personName}, would you like to learn some python today?`);
